51711c84f83f8aab35237af9af2dfbf433f8e132
Abhishek kumar <abhishek769@gmail.com>
2016-08-18 11:55:16 +0530

    adding new files.

---
+++ src/com/abhi/hellocontroller/HelloController.java
@@ -0,0 +1,17 @@
+/*package com.abhi.hellocontroller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class HelloController extends AbstractController {
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		ModelAndView modelAndView = new ModelAndView("HelloPage");
+		modelAndView.addObject("welcomeMessage", "Hi User Welcome to Spring MVC tutorial");
+		return modelAndView;
+	}
+}*/
--- src/com/abhi/hellocontroller/HelloControllerNew.java
+++ src/com/abhi/hellocontroller/HelloControllerNew.java
@@ -5,12 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/greet")
 public class HelloControllerNew {
 
 	@RequestMapping("/welcomeBack")
 	public ModelAndView helloWorld(){
 		ModelAndView modelAndView = new ModelAndView("HelloPage");
-		modelAndView.addObject("welcomeMessage", "This Message is from the new controller");
+		modelAndView.addObject("welcomeMessage", "This Message is from the new controller.");
+		return modelAndView;
+	}
+	
+	@RequestMapping("/hi")
+	public ModelAndView hiMsg(){
+		ModelAndView modelAndView = new ModelAndView("HelloPage");
+		modelAndView.addObject("welcomeMessage", "This Message is from second controller.");
 		return modelAndView;
 	}
 }
