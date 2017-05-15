d7a91ad90f4e2091b50d6e0868862e3f432e4a45
Abhishek kumar <abhishek769@gmail.com>
2017-05-15 21:07:58 +0530

    new files added

--- src/com/abhi/hellocontroller/HelloControllerNew.java
+++ src/com/abhi/hellocontroller/HelloControllerNew.java
@@ -1,11 +1,14 @@
 package com.abhi.hellocontroller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/greet")
+//@RequestMapping("/greet")
 public class HelloControllerNew {
 
 	@RequestMapping("/welcomeBack")
@@ -15,10 +18,19 @@ public class HelloControllerNew {
 		return modelAndView;
 	}
 	
-	@RequestMapping("/hi")
-	public ModelAndView hiMsg(){
+	@RequestMapping("/welcome/{userName}/{countryName}")
+	public ModelAndView hiMsg(@PathVariable("countryName") String country,  @PathVariable("userName") String username){
 		ModelAndView modelAndView = new ModelAndView("HelloPage");
-		modelAndView.addObject("welcomeMessage", "This Message is from second controller.");
+		modelAndView.addObject("welcomeMessage", "Welcome "+ username + " from "+country);
+		return modelAndView;
+	}
+	
+	@RequestMapping("/welcome/{userName}/{countryName}")
+	public ModelAndView hiNewMsg(@PathVariable Map<String,String> pathvars){ //  to use this map of path variable u need to use <mvc:annotation-drivers/>
+		String  username = pathvars.get("userName");
+		String  country = pathvars.get("countryName");
+		ModelAndView modelAndView = new ModelAndView("HelloPage");
+		modelAndView.addObject("welcomeMessage", "Welcome "+ username + " from "+country);
 		return modelAndView;
 	}
 }
